3736cfec7937b1b3dfce1457dacba5565a2334e1
floof09 <fapflores@gmail.com>
2024-05-23 15:30:47 +0800

    CHANGED TO DROPDOWN

--- purchase_devices.php
+++ purchase_devices.php
@@ -11,31 +11,35 @@ $devices = [
 if (!empty($devices)) {
     if (!isset($_POST['submit']) || empty($_POST['quantity'])) { // Display purchase form if form not submitted or quantity array is empty
         echo "<form action='' method='POST'>";
-        echo "<table>";
-        echo "<tr><th>Device Name</th><th>Price</th><th>Initial Quantity</th><th>Quantity to Buy</th></tr>";
-
+        echo "<label for='device'>Choose a device:</label>";
+        echo "<select name='device' id='device'>";
+        echo "<option value='' selected disabled>Select Device</option>";
         foreach ($devices as $device) {
-            $initialQuantity = isset($_SESSION['quantity'][$device['id']]) ? $_SESSION['quantity'][$device['id']] : 1;
-            echo "<tr>";
-            echo "<td>{$device['device_name']}</td>";
-            echo "<td>{$device['price']}</td>";
-            echo "<td>{$device['quantity']}</td>";
-            echo "<td><input type='number' name='quantity[{$device['id']}]' min='1' max='{$device['quantity']}' value='$initialQuantity' required></td>";
-            echo "</tr>";
+            echo "<option value='{$device['id']}'>{$device['device_name']}</option>";
         }
-
-        echo "</table>";
+        echo "</select>";
+        echo "<label for='quantity'>Quantity to Buy:</label>";
+        echo "<input type='number' name='quantity' id='quantity' min='1' required>";
         echo "<button type='submit' name='submit'>Submit</button>";
         echo "</form>";
         echo "<button onclick='window.location.href=\"index.php\";'>Back to Main Menu</button>"; // Button to go back to the main menu
     } elseif (isset($_POST['submit'])) { // Display confirmation message after purchase
-        foreach ($_POST['quantity'] as $id => $quantity) {
-            $_SESSION['quantity'][$id] = $quantity;
+        $deviceId = $_POST['device'];
+        $quantity = $_POST['quantity'];
+        
+        // Find the selected device
+        $selectedDevice = array_values(array_filter($devices, function ($device) use ($deviceId) {
+            return $device['id'] == $deviceId;
+        }))[0];
+
+        // Check if quantity is valid
+        if ($quantity > 0 && $quantity <= $selectedDevice['quantity']) {
+            echo "<p>Thank you for your purchase of {$quantity} {$selectedDevice['device_name']}!</p>";
+        } else {
+            echo "<p>Invalid quantity selected.</p>";
         }
 
-        echo "<p>Thank you for your purchase!</p>";
         echo "<button onclick='window.location.href=\"index.php\";'>Back to Main Menu</button>"; // Button to go back to the main menu
-        echo "<button onclick='window.location.href=\"purchase_devices.php\";'>Back to purchase devices</button>"; // Button to go back to the main menu
         session_unset(); // Clear session data after purchase confirmation
     }
 } else {
