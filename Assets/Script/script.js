44f6ea7173abf68890dcd464400e92beaa3398c0
Mar <153974951+Mar1154@users.noreply.github.com>
2024-05-26 17:05:11 +0800

    Add files via upload

---
+++ Assets/Script/script.js
@@ -0,0 +1,11 @@
+function home() {
+    window.location.href = "index.php";
+}
+function openAddDevice() {
+    window.location.href = "addDevice.php";
+}
+function openViewDevice() {
+    window.location.href = "viewDevice.php";
+}
+
+
